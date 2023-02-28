1cda2f26f73a45145f0a0a45817630be9e560f03
ankbhan <69941112+ankbhan@users.noreply.github.com>
2023-02-28 16:53:27 -0500

    few chanages

--- game.js
+++ game.js
@@ -77,7 +77,7 @@ new Phaser.Game({
     width: 1400, // Width of the game in pixels
     height: 600, // Height of the game in pixels
     backgroundColor: '#3498db', // The background color (blue)
-    scene: mainScene, // The name of the scene we created
+    scene:  mainScene, // The name of the scene we created
     physics: { default: 'arcade' }, // The physics engine to use
     parent: 'game', // Create the game inside the <div id="game"> 
 });
