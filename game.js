d1413452569e5b3e479458bbb36c218c34de3c25
ankbhan <69941112+ankbhan@users.noreply.github.com>
2023-02-28 16:48:46 -0500

    pixel changes

--- game.js
+++ game.js
@@ -54,8 +54,8 @@ class mainScene {
     }
     hit() {
         // Change the position x and y of the coin randomly
-        this.coin.x = Phaser.Math.Between(100, 600);
-        this.coin.y = Phaser.Math.Between(100, 300);
+        this.coin.x = Phaser.Math.Between(100, 1200);
+        this.coin.y = Phaser.Math.Between(100, 400);
       
         // Increment the score by 10
         this.score += 10;
@@ -72,11 +72,12 @@ class mainScene {
         });
       }
 }
-    new Phaser.Game({
-        width: 700, // Width of the game in pixels
-        height: 400, // Height of the game in pixels
-        backgroundColor: '#3498db', // The background color (blue)
-        scene: mainScene, // The name of the scene we created
-        physics: { default: 'arcade' }, // The physics engine to use
-        parent: 'game', // Create the game inside the <div id="game"> 
-      });
+
+new Phaser.Game({
+    width: 1400, // Width of the game in pixels
+    height: 600, // Height of the game in pixels
+    backgroundColor: '#3498db', // The background color (blue)
+    scene: mainScene, // The name of the scene we created
+    physics: { default: 'arcade' }, // The physics engine to use
+    parent: 'game', // Create the game inside the <div id="game"> 
+});
